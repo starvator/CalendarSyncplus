a94dfe3017ce93df42e5d074f0cac1540710bb3b
Tyler Ramsay <t_ramsa@MONO.ENCS.concordia.ca>
2015-11-23 14:04:43 -0500

    Refactoring 2 test cases for original code

---
+++ src/CalendarSyncPlus/RefactoringTests/RefactoringTwo/UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalendarSyncPlus.Domain.Models.Preferences;
+
+namespace RefactoringTests
+{
+
+    //Here be the template. To delete later -pmiriello
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void DailySyncTest()
+        {
+            DateTime dt = new DateTime(2015, 11, 23);
+            SyncFrequency a = new DailySyncFrequency();
+            System.Console.WriteLine(a.GetNextSyncTime(dt));
+            System.Console.WriteLine(a.ValidateTimer(dt));
+        }
+        public void WeeklySyncTest()
+        {
+            DateTime dt = new DateTime(2015, 11, 23);
+            SyncFrequency a = new WeeklySyncFrequency();
+            System.Console.WriteLine(a.GetNextSyncTime(dt));
+            System.Console.WriteLine(a.ValidateTimer(dt));
+        }
+        public void IntervalSyncTest()
+        {
+            DateTime dt = new DateTime(2015, 11, 23);
+            SyncFrequency a = new IntervalSyncFrequency();
+            System.Console.WriteLine(a.GetNextSyncTime(dt));
+            System.Console.WriteLine(a.ValidateTimer(dt));
+        }
+    }
+}
