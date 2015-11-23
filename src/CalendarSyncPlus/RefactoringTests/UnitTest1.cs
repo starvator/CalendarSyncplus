124ee3eabf0e652c518387fe348a1bde59a031ae
Philippe <pmiriello@gmail.com>
2015-11-23 13:28:14 -0500

    Added UnitTest Project
    Added UnitTest Project to allow testing of old project vs refactored
    project

---
+++ src/CalendarSyncPlus/RefactoringTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RefactoringTests
+{
+
+    //Here be the template. To delete later -pmiriello
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
